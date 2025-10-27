65c5f39dc817e5f9730f7bc30db2108d3a8ae373
Vince1993 <52530147+Vince1993@users.noreply.github.com>
2025-10-27 11:42:42 +0100

    some progess on trainingszeiten.html

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
     $("#trigram").click(slideInAndOut);
+    $('#link_to_top').click(scrollToTop);
 });
 
 function slideInAndOut() {
-    $(".main_navigation").toggle(500);
-}
+    $(".main_navigation").toggle(500);}
+
+function scrollToTop(){
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+    return false;}
+
